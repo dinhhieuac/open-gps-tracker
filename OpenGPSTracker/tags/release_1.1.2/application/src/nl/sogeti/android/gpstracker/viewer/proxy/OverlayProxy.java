8e137f249a1a9da77e3e9d90e3053e0e21fff543
rcgroot <rcgroot@43c4e576-f067-11dd-a979-a7afd4e285e1>
2011-03-27 09:09:39 +0000

    1.1.2 release

---
+++ OpenGPSTracker/tags/release_1.1.2/application/src/nl/sogeti/android/gpstracker/viewer/proxy/OverlayProxy.java
@@ -0,0 +1,8 @@
+package nl.sogeti.android.gpstracker.viewer.proxy;
+
+
+public interface OverlayProxy
+{
+   public com.google.android.maps.Overlay getGoogleOverlay();
+   public org.osmdroid.views.overlay.Overlay getOSMOverlay();
+}
