60e76ef1d3e1e7ed8cc73437e2132001f030ec4d
rcgroot <rcgroot@43c4e576-f067-11dd-a979-a7afd4e285e1>
2011-04-28 15:55:40 +0000

    Bugfix release without Breadcrumbs integration

---
+++ OpenGPSTracker/branches/release_1.1.3/application/src/nl/sogeti/android/gpstracker/viewer/proxy/OverlayProxy.java
@@ -0,0 +1,9 @@
+package nl.sogeti.android.gpstracker.viewer.proxy;
+
+
+public interface OverlayProxy
+{
+   public com.google.android.maps.Overlay getGoogleOverlay();
+   public org.osmdroid.views.overlay.Overlay getOSMOverlay();
+   public void closeResources();
+}
